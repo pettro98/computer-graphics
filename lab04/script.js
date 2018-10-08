716edd178bdd7a01055caa1d2b97def987095e15
pettro98 <ua.skywalker@yandex.ru>
2018-10-08 13:22:29 +0300

    some changes to lab04

--- lab04/script.js
+++ lab04/script.js
@@ -51,19 +51,18 @@ function processImage() {
         var sum = Math.sqrt(sumX[0] * sumX[0] + sumY[0] * sumY[0]) +
             Math.sqrt(sumX[1] * sumX[1] + sumY[1] * sumY[1]) +
             Math.sqrt(sumX[2] * sumX[2] + sumY[2] * sumY[2]);
-        sum /= 4;
+        sum /= 10; // brightness
+        
         var c2 = coord(x, y, 0);
         newData[c2] = sum;
         newData[c2 + 1] = sum;
         newData[c2 + 2] = sum;
         newData[c2 + 3] = 255;
     }
-
     for (var y = 0; y < H; ++y) {
         for (var x = 0; x < W; ++x) {
             filter(x, y, beforeImData.data, afterImData.data);
         }
     }
-
     afterCtx.putImageData(afterImData, 0, 0);
 }
