3b08cd7b312e331bb0804ad4b904ebd7895c1d59
pettro98 <ua.skywalker@yandex.ru>
2018-10-18 22:35:44 +0300

    optimized line algorithm

--- lab03/script.js
+++ lab03/script.js
@@ -6,23 +6,30 @@ var waiting = null;
 var prevX = 0;
 var prevY = 0;
 
-function lineTo(x, y) {
-    x = Math.floor(x / gridSize);
-    y = Math.floor(y / gridSize);
-    var dx = Math.abs(x - prevX);
-    var dy = Math.abs(y - prevY);
-    var err = 0;
-    var derr = dy / dx;
-    var cy = prevY;
-    var dir = (y - prevY > 0) ? 1 : -1;
-    for (var cx = prevX; cx <= x; ++cx) {
-        ctx.fillRect(cx * gridSize, cy * gridSize, gridSize, gridSize);
-        err = err + derr;
-        if (err >= 0.5) {
-            cy = cy + dir;
-            err = err - 1;
+// TERNARY OPERATOR CHAOS
+function line(x1, y1, x2, y2) {
+    var deltaX = x2 - x1;
+    var deltaY = y2 - y1;
+    var dx = deltaX / deltaY;
+    var dy = deltaY / deltaX;
+    var eps = 0;
+    var horizontal = Math.abs(deltaX) >= Math.abs(deltaY);
+    // just flowers, berries later :))
+    var dcx = (horizontal) ? 0 : ((deltaX >= 0) ? 1 : -1);
+    var dcy = (horizontal) ? ((deltaY >= 0) ? 1 : -1) : 0;
+    var incX = (horizontal) ? ((deltaX >= 0) ? 1 : -1) : 0;
+    var incY = (horizontal) ? 0 : ((deltaY >= 0) ? 1 : -1);
+    // OHHH YEEEAH, BABY, CRY, I WANT TO SEE YOUR TEARS OF BLOOD XDDDD
+    for (var cx = x1, cy = y1; (horizontal) ? ((incX > 0) ? cx <= x2 : cx >= x2) : ((incY > 0) ? cy <= y2 : cy >= y2); cx += incX, cy += incY) {
+        if (Math.abs(eps) >= 0.5) {
+            eps -= (eps >= 0) ? 1 : -1;
+            cy += dcy;
+            cx += dcx;
         }
+        ctx.fillRect(cx * gridSize, cy * gridSize, gridSize, gridSize);
+        eps += (horizontal) ? dy : dx;
     }
+    // approx. 350 rubber ducklings were destroyed while debugging THIS
 }
 
 function circleTo(x, y) {
@@ -55,12 +62,12 @@ canvas.addEventListener("click", function (event) {
     event.preventDefault();
     if (!waiting) {
         waiting = "LMB";
-        prevX = Math.floor(event.offsetX/gridSize);
-        prevY = Math.floor(event.offsetY/gridSize);
+        prevX = Math.floor(event.offsetX / gridSize);
+        prevY = Math.floor(event.offsetY / gridSize);
         return;
     }
     if (waiting === "LMB") {
-        lineTo(event.offsetX, event.offsetY);
+        line( prevX, prevY, Math.floor(event.offsetX / gridSize) , Math.floor(event.offsetY / gridSize));
         waiting = null;
     }
 });
